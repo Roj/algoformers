1cd48dd40499a112d08cd57f21fabfa13f2c24b9
FEDE <FEDE@FEDE-PC>
2016-05-30 12:27:28 -0300

    Excepción para ataque invalido y test para ataques entre algoformers

---
+++ test/algoformers/AlgoformerTest.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package algoformers;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+/**
+ *
+ * @author joaquintz
+ */
+public class AlgoformerTest {
+    @Test(expected=NoSuperponibleException.class)
+    public void testSuperponerLanzaExcepcion() {
+        FabricaAlgoformers fabrica = new FabricaAlgoformers();	
+        Algoformer alf = fabrica.crearOptimus();
+        
+        alf.superponer(alf);
+    }
+    
+    @Test(expected=AtaqueInvalidoException.class)
+    public void testAtacarAlgoformerMismoBando() {
+    	FabricaAlgoformers fabrica = new FabricaAlgoformers();
+    	Algoformer alf = fabrica.crearOptimus();
+    	Algoformer otro_alf = fabrica.crearOptimus();
+    	
+    	alf.atacar(otro_alf);
+    }
+}
