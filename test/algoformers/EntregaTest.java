2baa5a82d396e493e19fc225faeb9f2c6d480b45
FEDE <FEDE@FEDE-PC>
2016-06-02 01:48:08 -0300

    Test 4 entrega agregado

--- test/algoformers/EntregaTest.java
+++ test/algoformers/EntregaTest.java
@@ -90,6 +90,52 @@ public class EntregaTest {
         
         Assert.assertTrue(optimus.obtenerPosicion() == nuevaPosicion);
     }
-
+    @Test
+    public void test04CrearJuegoConAlgoformersEnEsquivasOpuestasYChispaEnCentro(){
+        String nombre1 = "Juan";
+        String nombre2 = "John";
+        
+        Juego juego = new Juego(nombre1,nombre2,20,20);
+        
+        Jugador jugador1 = juego.obtenerJugadorActual();
+        Jugador jugador2 = juego.obtenerJugadorEnEspera();
+        
+        Tablero tablero = juego.obtenerTablero();
+        
+        Algoformer optimus = jugador1.obtenerAlgoformer(0);
+        Algoformer bumblebee = jugador1.obtenerAlgoformer(1);
+        Algoformer ratchet = jugador1.obtenerAlgoformer(2);
+        
+        Algoformer megatron = jugador2.obtenerAlgoformer(0);
+        Algoformer bonecrusher = jugador2.obtenerAlgoformer(1);
+        Algoformer frenzy = jugador2.obtenerAlgoformer(2);
+        
+        Ubicable chispa = tablero.obtenerUbicable(new Posicion(10, 10));
+               
+        Posicion esquinaInferiorIzqBorde = new Posicion(0,0);
+        Posicion esquinaInferiorIzqSuperior = new Posicion(0,1);
+        Posicion esquinaInferiorIzqFrontal = new Posicion(1,0);
+        
+        Posicion esquinaSuperiorDerBorde = new Posicion(19,19);
+        Posicion esquinaSuperiorDerInferior = new Posicion(19,18);
+        Posicion esquinaSuperioDerTrasera = new Posicion(18,19);
+        
+        tablero.colocarAlgoformer(esquinaInferiorIzqBorde, optimus);
+        tablero.colocarAlgoformer(esquinaInferiorIzqSuperior, bumblebee);
+        tablero.colocarAlgoformer(esquinaInferiorIzqFrontal, ratchet);
+        
+        tablero.colocarAlgoformer(esquinaSuperiorDerBorde, megatron);
+        tablero.colocarAlgoformer(esquinaSuperiorDerInferior, bonecrusher);
+        tablero.colocarAlgoformer(esquinaSuperioDerTrasera, frenzy);
+        
+        Assert.assertTrue(optimus.obtenerPosicion() == esquinaInferiorIzqBorde);
+        Assert.assertTrue(bumblebee.obtenerPosicion() == esquinaInferiorIzqSuperior);
+        Assert.assertTrue(ratchet.obtenerPosicion() == esquinaInferiorIzqFrontal);
+        
+        Assert.assertTrue(megatron.obtenerPosicion() == esquinaSuperiorDerBorde);
+        Assert.assertTrue(bonecrusher.obtenerPosicion() == esquinaSuperiorDerInferior);
+        Assert.assertTrue(frenzy.obtenerPosicion() == esquinaSuperioDerTrasera);     
+        Assert.assertTrue(chispa instanceof ChispaSuprema); 
+    }
     
 }
