9cd3f66109a982a5ff9648d3d9f90e6b9db0c680
FEDE <FEDE@FEDE-PC>
2016-06-16 19:55:02 -0300

    Up accion realizar ataque

---
+++ src/algoformers/controlador/AccionRealizarAtaque.java
@@ -0,0 +1,39 @@
+package algoformers.controlador;
+
+import java.util.List;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import algoformers.modelo.Juego;
+import algoformers.vista.Casilla;
+import algoformers.vista.ContenedorJuego;
+
+public class AccionRealizarAtaque implements EventHandler<ActionEvent> {
+
+	ContenedorJuego contenedorJuego;
+	Juego juego;
+    
+    public AccionRealizarAtaque(ContenedorJuego contJuego, Juego juego) {
+        this.contenedorJuego = contJuego;
+        this.juego = juego;
+    }
+
+    @Override
+    public void handle(ActionEvent event) {
+    	/* Todo lo de atacar al algoformer
+    	
+    	
+    	
+    	*/
+    	Casilla casillaActual = this.contenedorJuego.getCasillaActual();
+    	List<Casilla> casillasPosiblesAtaque = this.contenedorJuego.getCasillasPosiblesAtaque(casillaActual);
+    	
+        this.contenedorJuego.pasarTurno();
+        this.contenedorJuego.cambiarEstadoCasilla(new AccionMarcarCasilla(this.contenedorJuego, juego));
+        
+    	this.contenedorJuego.dejarDeMostrarCasillas(casillasPosiblesAtaque);
+
+    }
+    
+}
+
